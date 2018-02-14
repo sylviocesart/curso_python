a08fa993d73de6dcc0d0a84c5e780102a998cd91
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-02-14 18:11:17 -0200

    Adicionado Exercício 01 da aula06.

---
+++ Ignorância_Zero/aula06/exercicio01.py
@@ -0,0 +1,8 @@
+num1, num2 = 15,10
+print("x =",num1)
+print("y =",num2)
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"/",num2,"=",num1/num2)
+print(num1,"//",num2,"=",num1//num2)
+print(num1,"resto",num2,"=",num1%num2)
