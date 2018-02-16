1e78ab48394dddba3c9f96f9e5a3ab092f169356
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-02-15 22:55:43 -0200

    Adicionado aula10.

---
+++ Ignorância_Zero/aula10/Comparacoes_Multiplas.py
@@ -0,0 +1,30 @@
+"""
+# Aprendendo trabalhar com comparações múltiplas
+# Verificando se a idade é menor ou maior de idade ou idoso
+valor = int(input("Digite sua idade: "))
+if 0 < valor < 18:
+    print("Você é menor de idade!!\n\n")
+else:
+    if valor > 70:
+        print("Você é idoso!!\n\n")
+    else:
+        print("Você é maior de idade\n\n")
+
+## Vamos atribuir uma condição, se for maior ou igual a 18 e menor que 70
+## então, terá direito a um benefício
+# Caso seja diferente, não terá o benefício
+idade = int(input("Digite sua idade: "))
+
+if 18 <= idade < 70:
+    print("Você terá direito ao benefício\n\n")
+else:
+    print("Você não terá direito ao benefício\n\n")
+
+## Agora, juntando ambos e formando um só programa
+"""
+idade = int(input("Digite sua idade: "))
+if 18 <= idade < 70:
+    print("Você terá o benefício, pois é maior de idade\n\n")
+else:
+    print("Você não terá direito ao benefício, pois é idoso\n\n")
+ 
