4fef51f22eca8b188e0904e71fca07e2b55c44c7
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-02-17 19:19:41 -0200

    inicio_aula12

---
+++ Ignorância_Zero/aula12/Multiplos_if_vs_elif.py
@@ -0,0 +1,7 @@
+"""
+Quando que devemos usar vários if ou vários elif??
+
+Normalmente as comparações no elif estão associadas as mesmas
+variáveis em todos os elif
+
+Já o if é usado para variáveis diferentes
