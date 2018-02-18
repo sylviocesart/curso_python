6110408dbce8d501942eb49171a8f6a4a7a85ed1
Sylvio Cesar <sylvio.cesart@gmail.com>
2018-02-18 00:21:17 -0300

    Adicionado aula13.

---
+++ Ignorância_Zero/aula13/Est.Repeticao_While-I.py
@@ -0,0 +1,19 @@
+"""
+while == enquanto
+
+Verifica se alguma coisa for VERDADEIRA, e executa enquanto for
+VERDADE.
+
+A sintaxe é:
+    while aluma coisa: 
+        execute isso
+
+"""
+# Número máximo
+num = 10
+
+# Variável contadora
+cont = 0
+while cont < num:
+    print(cont)
+    cont += 1
