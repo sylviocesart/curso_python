b5e9d1b6660969667c13b466924b5424f5c52aba
Sylvio César <sylvio@MB-Sylvio.local>
2018-02-24 00:16:44 -0300

    Atualizada aula15.

--- Ignorância_Zero/aula15/Dois_atalhos.py
+++ Ignorância_Zero/aula15/Dois_atalhos.py
@@ -1,6 +1,6 @@
 """
 Dois atalhos usados em operações matemática
-"""
+
 n = 10
 cont = 0
 while cont < n:
@@ -8,6 +8,7 @@ while cont < n:
     cont = cont + 1
 
 ## Realizando o atalho, a operação acima ficaria assim:
+"""
 n = 10
 cont = 0
 print("Novo cálculo usando atalho na operação")
